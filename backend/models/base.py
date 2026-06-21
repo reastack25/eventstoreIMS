3b1db0251f26d485bf7785323367f3dac90c9efb
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-21 21:00:15 +0300

    feat: created a reusable base model

---
+++ backend/models/base.py
@@ -0,0 +1,11 @@
+# models/base.py
+
+from extensions import db
+from datetime import datetime
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer,primary_key=True)
+    created_at = db.Column(db.DateTime,default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime,default=datetime.utcnow,onupdate=datetime.utcnow)
