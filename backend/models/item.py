2d5179e41b83bb7673ff1d361042740dd3c20de2
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-21 21:15:11 +0300

    feat: created a  inventory item model

---
+++ backend/models/item.py
@@ -0,0 +1,16 @@
+# models/item.py
+
+from extensions import db
+from models.base import BaseModel
+
+class Item(BaseModel):
+    __tablename__ = "items"
+    
+    code = db.Column(db.String(100),unique=True)
+    name = db.Column(db.String(255),nullable=False)
+    category_id = db.Column(db.Integer,db.ForeignKey("categories.id"))
+    unit = db.Column(db.String(50))
+    quantity = db.Column(db.Integer,default=0)
+    available = db.Column(db.Integer,default=0)
+    status = db.Column(db.String(50),default="ACTIVE")
+    image_url = db.Column(db.String(500))
