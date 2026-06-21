754966ea18540d6aaec3ad67416d9af9e9ddcc83
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-21 20:18:47 +0300

    feat: implement application factory pattern

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from extensions import (db,jwt,migrate,cors)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(
+        "config.development.DevelopmentConfig"
+    )
+
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+    cors.init_app(app)
+
+    from routes import register_routes
+    register_routes(app)
+
+    return app
