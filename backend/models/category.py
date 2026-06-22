743d5231ad4ae57ddda904b0a9af1b365ceb2660
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-22 08:20:44 +0300

    feat: create category model

---
+++ backend/models/category.py
@@ -0,0 +1,11 @@
+from extensions import db
+from models.base import BaseModel
+
+
+class Category(BaseModel):
+    __tablename__ = "categories"
+
+    name = db.Column(db.String(100),unique=True,nullable=False)
+    description = db.Column(db.Text)
+
+    items = db.relationship("Item",backref="category",lazy=True)
