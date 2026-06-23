ba17b7adb2a4757803719e496cc313d33b461512
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-23 13:47:29 +0300

    feat: add create category endpoint

--- backend/api/categories/routes.py
+++ backend/api/categories/routes.py
@@ -9,3 +9,13 @@ category_bp = Blueprint("categories",__name__,url_prefix="/api/categories")
 
 category_schema = CategorySchema()
 categories_schema = CategorySchema(many=True)
+
+@category_bp.route("/", methods=["POST"])
+def create_category():
+
+    data = request.get_json()
+    category = CategoryService.create_category(data)
+
+    return (
+        category_schema.dump(category),201
+    )
