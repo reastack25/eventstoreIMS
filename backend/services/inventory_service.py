8d6b3fb1a008833eb4e4f89472036a9daacd0fb6
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-24 09:41:08 +0300

    feat: implement inventory business logic

--- backend/services/inventory_service.py
+++ backend/services/inventory_service.py
@@ -4,5 +4,18 @@ from repositories.inventory_repo import InventoryRepository
 class InventoryService:
 
     @staticmethod
+    def create_item(data):
+
+        item = Item(
+            code=data["code"],
+            name=data["name"],
+            quantity=data.get("quantity", 0),
+            available=data.get("available", 0),
+            status=data.get("status", "ACTIVE"),
+            category_id=data["category_id"]
+        )
+        return InventoryRepository.create(item)
+
+    @staticmethod
     def get_items():
         return InventoryRepository.get_all()
