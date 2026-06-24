5dae6233d2fdb2961531e9e132cb6b4ebd17c7ce
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-06-24 09:32:53 +0300

    feat: add inventory item schema

--- backend/schemas/inventory_schema.py
+++ backend/schemas/inventory_schema.py
@@ -2,9 +2,10 @@ from marshmallow import Schema, fields
 
 
 class ItemSchema(Schema):
-    id = fields.Int()
-    name = fields.Str()
-    code = fields.Str()
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    code = fields.Str(required=True)
     quantity = fields.Int()
     available = fields.Int()
     status = fields.Str()
+    category_id = fields.Int(required=True)
