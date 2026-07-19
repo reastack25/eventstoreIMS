74e9427777248ffd1bdd070efef86be6896edf44
reastack25 <221689087+reastack25@users.noreply.github.com>
2026-07-19 21:58:52 +0300

    fix: add get_all_paginated with search to ItemRepository

--- backend/repositories/inventory_repo.py
+++ backend/repositories/inventory_repo.py
@@ -1,11 +1,26 @@
 from models.item import Item
 from extensions import db
 
+class ItemRepository:
 
-class InventoryRepository:
     @staticmethod
-    def get_all():
-        return Item.query.all()
+    def get_all_paginated(page=1, per_page=20, search=None):
+        query = Item.query
+
+        if search:
+            query = query.filter(
+                Item.name.ilike(f"%{search}%")
+            )
+
+        return query.paginate(
+            page=page,
+            per_page=per_page,
+            error_out=False
+        )
+
+    @staticmethod
+    def get_by_id(item_id):
+        return Item.query.get(item_id)
 
     @staticmethod
     def create(item):
@@ -14,5 +29,5 @@ class InventoryRepository:
         return item
 
     @staticmethod
-    def get_by_id(item_id):
-        return Item.query.get(item_id)
+    def get_all():
+        return Item.query.all()
